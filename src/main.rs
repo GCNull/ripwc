cad6b024b2f30c6acdbcb2d04d9fe4a0e18fcb8a
GCNull <curry.garrett@gmail.com>
2022-06-30 00:22:02 +0100

    Can now use linux '*' wildcard when specifying files

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use walkdir::WalkDir;
 #[derive(Parser)]
 #[clap(author="GCNull", version=env!("CARGO_PKG_VERSION"), about="A rewrite of the GNU coreutils 'wc' tool.", long_about = None)]
 struct Args {
-    #[clap(help="The file or folder to scan", required=true)]
-    file: String,
+    #[clap(value_parser, help="The file or folder to scan", required=true)]
+    file: Vec<String>,
 
     #[clap(short='c', long, help="print the byte counts")]
     bytes: bool,
@@ -28,12 +28,15 @@ struct Args {
     #[clap(short='r', long, help="recursively search through folders and files")]
     recursive: bool,
 
-    #[clap(short='v', long, help="print verbose output")]
+    #[clap(short='v', help="print verbose output")]
     verbose: bool,
+
+    #[clap(long="vv", help="print extra verbose output")]
+    verbose2: bool,
 }
 
 fn main() {
-    let mut app = Args::parse();
+    let mut app: Args = Args::parse();
     let mut result = String::new();
     let mut total_byte_count: usize = 0;
     let mut total_char_count: usize = 0;
@@ -42,79 +45,82 @@ fn main() {
     // let mut total_max_line_counter: usize = 0;
 
     // Recursively go through directories. If recursion is false then stop after the first file is found
-    for entry in WalkDir::new(&app.file) {
-        match entry {
-            Ok(path) => {
-                if app.verbose && !path.path().is_file() { println!("Searching {:?}", path.path()) }
-                if path.path().is_file() {
-                    if app.verbose { println!("Found {:?}", path.path().display()) }
-                    let i = path.path();
+    for i in &app.file {
+        for entry in WalkDir::new(&i) {
+            match entry {
+                Ok(path) => {
+                    if app.verbose && !path.path().is_file() { println!("Searching {:?}", path.path()) }
+                    if path.path().is_file() {
+                        if app.verbose { println!("Found {:?}", path.path().display()) }
+                        let i = path.path();
 
-                    if app.verbose { println!("Opening {:?}", i) }
-                    let mut byte_count = 0;
-                    let mut char_count = 0;
-                    let mut word_count = 0;
-                    let mut newline_count = 0;
-                    // let mut max_line_counter = 0;
+                        if app.verbose { println!("Opening {:?}", i) }
+                        let mut byte_count = 0;
+                        let mut char_count = 0;
+                        let mut word_count = 0;
+                        let mut newline_count = 0;
+                        // let mut max_line_counter = 0;
 
-                    if let Ok(open_f) = File::open(i) {
-                        let data = open_f.metadata().unwrap().len();
-                        let reader = BufReader::new(open_f);
-                        for j in reader.bytes() {
-                            // Switch flags to true if all false. Default settings on 'wc'
-                            if !app.bytes && !app.lines && !app.words && !app.chars {
-                                app.bytes = true;
-                                app.lines = true;
-                                app.words = true;
+                        if let Ok(open_f) = File::open(i) {
+                            let data = open_f.metadata().unwrap().len();
+                            let reader = BufReader::new(open_f);
+                            for j in reader.bytes() {
+                                // Switch flags to true if all false. Default settings on 'wc'
+                                if !app.bytes && !app.lines && !app.words && !app.chars {
+                                    app.bytes = true;
+                                    app.lines = true;
+                                    app.words = true;
+                                }
+
+                                if app.bytes && !app.lines && !app.words && !app.chars {
+                                    byte_count = data as usize;
+                                    break;
+                                } else if app.bytes { byte_count = data as usize; }
+
+                                // if app.verbose { println!("0x{:X}", &j.as_ref().unwrap()) }
+
+                                if app.chars && j.as_ref().unwrap() != &0x0 {
+                                    char_count += 1;
+                                    // if app.verbose { println!("Found char 0x{:X}", &j.as_ref().unwrap()) }
+                                }
+                                if app.lines && j.as_ref().unwrap() == &0xA { // newline counter
+                                    newline_count += 1;
+                                    // if app.verbose { println!("Found newline 0x{:X}", &j.as_ref().unwrap()) }
+                                }
+                                if app.words && j.as_ref().unwrap().is_ascii_whitespace() {
+                                    word_count += 1;
+                                    // if app.verbose { println!("Found word 0x{:X}", &j.as_ref().unwrap()) }
+                                }
+
+                                // if app.max_line_length && !j.as_ref().unwrap().is_ascii_whitespace() {
+                                //     max_line_counter += 1;
+                                // }
+                                // if app.max_line_length && j.as_ref().unwrap().is_ascii_whitespace() && max_line_counter > max_line_length {
+                                //     max_line_length = max_line_counter;
+                                //     max_line_counter = 0;
+                                // }
                             }
-
-                            if app.bytes && !app.lines && !app.words && !app.chars {
-                                byte_count = data as usize;
-                                break;
-                            } else if app.bytes { byte_count = data as usize; }
-
-                            if app.verbose { println!("0x{:X}", &j.as_ref().unwrap()) }
-
-                            if app.chars && j.as_ref().unwrap() != &0x0 {
-                                char_count += 1;
-                                if app.verbose { println!("Found char 0x{:X}", &j.as_ref().unwrap()) }
-                            }
-                            if app.lines && j.as_ref().unwrap() == &0xA { // newline counter
-                                newline_count += 1;
-                                if app.verbose { println!("Found newline 0x{:X}", &j.as_ref().unwrap()) }
-                            }
-                            if app.words && j.as_ref().unwrap().is_ascii_whitespace() {
-                                word_count += 1;
-                                if app.verbose { println!("Found word 0x{:X}", &j.as_ref().unwrap()) }
-                            }
-
-                            // if app.max_line_length && !j.as_ref().unwrap().is_ascii_whitespace() {
-                            //     max_line_counter += 1;
-                            // }
-                            // if app.max_line_length && j.as_ref().unwrap().is_ascii_whitespace() && max_line_counter > max_line_length {
-                            //     max_line_length = max_line_counter;
-                            //     max_line_counter = 0;
-                            // }
+                            total_byte_count += byte_count;
+                            total_char_count += char_count;
+                            total_word_count += word_count;
+                            total_newline_count += newline_count;
+                            // mut total_max_line_counter += max_line_counter;
                         }
-                        total_byte_count += byte_count;
-                        total_char_count += char_count;
-                        total_word_count += word_count;
-                        total_newline_count += newline_count;
-                        // mut total_max_line_counter += max_line_counter;
-                    }
 
-                    if newline_count > 0 { result.push_str(&format!("newlines: {}", newline_count)) }
-                    if word_count > 0 { result.push_str(&format!(" words: {}", word_count)) }
-                    if char_count > 0 { result.push_str(&format!(" chars: {}", char_count)) }
-                    if byte_count > 0 { result.push_str(&format!(" bytes: {}", byte_count)) }
-                    // if max_line_length > 0 { result.push_str(&format!(" max line length: {}", max_line_length)) }
-                    println!("{} {:?}", result.trim(), i);
-                    if !app.recursive { break }
-                    result.clear();
+                        if newline_count > 0 { result.push_str(&format!("newlines: {}", newline_count)) }
+                        if word_count > 0 { result.push_str(&format!(" words: {}", word_count)) }
+                        if char_count > 0 { result.push_str(&format!(" chars: {}", char_count)) }
+                        if byte_count > 0 { result.push_str(&format!(" bytes: {}", byte_count)) }
+                        // if max_line_length > 0 { result.push_str(&format!(" max line length: {}", max_line_length)) }
+                        println!("{} {:?}", result.trim(), i);
+                        result.clear();
+                        if !app.recursive { break }
+                    }
                 }
+                Err(e) => eprintln!("Error: {}", e),
             }
-            Err(e) => eprintln!("Error: {}", e),
         }
+        if !app.recursive && app.file.len() > 1 { continue } else if !app.recursive && app.file.len() == 1 { break }
     }
     if total_newline_count > 0 { result.push_str(&format!("newlines: {}", total_newline_count)) }
     if total_word_count > 0 { result.push_str(&format!(" words: {}", total_word_count)) }
